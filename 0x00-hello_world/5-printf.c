f2de30ffcdee22e5c85620f0278746cdc763ff95
Fabrice Mehouelley <fabricemehouelley43@gmail.com>
2022-06-26 02:47:59 -0700

    hello world

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * use the function printf
+ * you are not allowed to use the function puts
+ * Return: Always 0 (success)
+ * your program should compile whitout warning using the option -Wall gcc
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
